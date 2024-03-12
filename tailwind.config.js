3691c39e2e7c4d973e34f28ce45ca0c78bb08b30
adelineafgr <adelineafigar@gmail.com>
2024-03-12 22:34:53 +0700

    hero section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,17 @@
 module.exports = {
   content: ["index.html"],
   theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
     extend: {
       colors: {
         indigo: '#1e1b4b',
+        pink: '#e879f9',
+      },
+      screens: {
+        '2xl': '1320px',
       }
     },
   },
