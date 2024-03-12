8fda31959263de4245efdf12a0df183aecc12b37
adelineafgr <adelineafigar@gmail.com>
2024-03-12 17:33:15 +0700

    introduction paragraph

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        indigo: '#1e1b4b',
+      }
+    },
+  },
+  plugins: [],
+}
